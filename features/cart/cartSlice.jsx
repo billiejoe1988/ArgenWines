3731a62cde2187dd2a1811c8dbd6c24e1d67217c
MauriArbelaiz <mauricio.arbelaiz@gmail.com>
2024-03-15 19:27:52 -0300

    FIX, console log

--- features/cart/cartSlice.jsx
+++ features/cart/cartSlice.jsx
@@ -24,7 +24,6 @@ export const cartSlice = createSlice({
             }
             
             state.total = state.items.reduce((acc,item)=> acc = acc + (item.price * item.quantity),0)
-            console.log(state)
         },
         deleteCartItem:(state,actions) =>{
             state.items = state.items.filter((item)=> item.id !== actions.payload)
